2fc8ebd1b78e724e082313d3f7a57e384ed337d2
Boris Reuderink <boris@cortext.nl>
2024-10-18 12:42:17 +0000

    Add first passing scan!

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -1,28 +1,59 @@
+# %%
+import keras
+from operator import add, mul
 from keras import ops
+import numpy as np
 import pytest
 
-def merge(prev, curr):
-    H_prev, Z_prev = prev
-    H_curr, Z_curr = curr
-    H = H_prev + Z_curr * (H_curr - H_prev)
 
-    # FIXME: Is maximum the right we to combine the gates?
-    Z = ops.maximum(Z_prev, Z_curr)
-    # print(f"{H_prev=}\n{H_curr=}\n{Z_curr=}\n=>\n{H=}\n{Z=},\n")
-    return H, Z
+def recurrence_op(i, j):
+    c_ia, c_ib = i
+    c_ja, c_jb = j
+
+    star = mul
+    companion = mul
+    plus = add
+    return (companion(c_ia, c_ja), plus(star(c_ib, c_ja), c_jb))
 
 
-def unroll(X, Z):
-    # TODO: make work on vectors.
-    H, _ = ops.associative_scan(merge, (X, Z))
+merge = recurrence_op
+
+
+def test_associative():
+    # f(a, f(b, c)) == f( f(a, b), c).
+
+    X = np.random.randn(3)
+    G = np.random.rand(3)
+    a, b, c = zip(X, G)
+
+    fn = merge
+    foldl = fn(fn(a, b), c)
+    foldr = fn(a, fn(b, c))
+
+    assert foldl == pytest.approx(foldr)
+
+
+def unroll(X, Z, axis=-2):
+    A, B = (1 - Z), Z * X
+    _, H = ops.associative_scan(merge, (A, B), axis=axis)
     return H
 
-@pytest.mark.parametrize("n", [10, 100])
-@pytest.mark.parametrize("position", range(10))
-def test_sparse_update(n, position):
-    X = ops.arange(n)
-    Z = ops.where(X == position, 1, 0)
-    Y_actual = unroll(X, Z)
-    Y_desired = ops.where(X < position, 0, position)
-    assert (Y_actual == Y_desired).all()
 
+@pytest.mark.parametrize("b", [1])
+@pytest.mark.parametrize("n", [10])
+@pytest.mark.parametrize("d", [1])
+def test_unroll(b, n, d):
+    X = np.arange(n, dtype=float)[None, :, None]
+    Z = np.full_like(X, fill_value=0.1)
+
+    print(X.squeeze())
+    print(Z.squeeze())
+
+    H_desired = np.zeros_like(X)
+    h = np.zeros(d, dtype=np.float32)
+    for i in range(n):
+        h = h + Z[:, i, :] * (X[:, i, :] - h)
+        H_desired[:, i, :] = h
+
+    H_actual = unroll(X, Z, axis=1)
+    np.testing.assert_allclose(H_actual.squeeze(), H_desired.squeeze(), rtol=1e-4)
