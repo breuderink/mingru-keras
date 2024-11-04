64ba383e5c2a2854e0f2395ee3d701e7f067c914
Boris Reuderink <boris@cortext.nl>
2024-11-04 09:08:56 +0100

    Add tensorflow metal dependencies and remove conditional from MinGRU call.

--- src/mingru_keras/core.py
+++ src/mingru_keras/core.py
@@ -37,15 +37,10 @@ class MinGRU(keras.Layer):
         self.gate.build(input_shape)
         self.candidate.build(input_shape)
 
-    def call(self, X, method="blellochs"):
+    def call(self, X):
         Z = self.gate(X)
         H_tilde = self.candidate(X)
-        if method == "blellochs":
-            H = Blellochs_method(H_tilde, Z)
-        elif method == "sequential":
-            H = sequential_method(H_tilde, Z)
-        else:
-            raise NotImplementedError(f"Method {method} is not implemented!")
+        H = Blellochs_method(H_tilde, Z)
 
         return H
 
